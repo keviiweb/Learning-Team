6a0d1e4239ca50066683d7b411332ea0ef1b7504
ezra1010101 <89738860+ezra1010101@users.noreply.github.com>
2021-09-13 11:51:22 +0800

    Code Cleanup

--- Introduction to Javascript/Submission Folder/EzraTio_nric_challenge.js
+++ Introduction to Javascript/Submission Folder/EzraTio_nric_challenge.js
@@ -2,24 +2,21 @@ function nric_checker(nric) {
   var sum = 0;
   var dict1 = { 0: "J", 1: "Z", 2: "I", 3: "H", 4: "G", 5: "F", 6: "E", 7: "D", 8: "C", 9: "B", 10: "A", };
   var dict2 = { 0: "X", 1: "W", 2: "U", 3: "T", 4: "R", 5: "Q", 6: "P", 7: "N", 8: "M", 9: "L", 10: "K", };
-  var splitted = nric.split("");
-  if (splitted.length != 9) {
+  if (nric.length != 9) {
     return "Invalid length";
   }
-  if (!['S','T','F','G'].includes(splitted[0].toUpperCase())) {
+  if (!['S','T','F','G'].includes(nric[0])) {
     return "Invalid first letter";
   }
-  sum = parseInt(splitted[1]) * 2 + parseInt(splitted[2]) * 7 + parseInt(splitted[3]) * 6 + parseInt(splitted[4]) * 5 
-        + parseInt(splitted[5]) * 4 + parseInt(splitted[6]) * 3 + parseInt(splitted[7]) * 2;
-  if (splitted[0] == "T" || splitted[0] == "G") {
-    sum += 4;
-  }
-  if (splitted[0] == "T" || splitted[0] == "S") {
-    if (dict1[sum % 11] != splitted[8]) {
+  sum = parseInt(nric[1]) * 2 + parseInt(nric[2]) * 7 + parseInt(nric[3]) * 6 + parseInt(nric[4]) * 5 
+        + parseInt(nric[5]) * 4 + parseInt(nric[6]) * 3 + parseInt(nric[7]) * 2;
+  sum = nric[0] == "T" || nric[0] == "G" ? (sum += 4) % 11 : sum % 11;
+  if (nric[0] == "T" || nric[0] == "S") {
+    if (dict1[sum] != nric[8]) {
       return "Invalid nric checksum";
     }
-  } else if (splitted[0] == "F" || splitted[0] == "G") {
-    if (dict2[sum % 11] != splitted[8]) {
+  } else {
+    if (dict2[sum] != nric[8]) {
       return "Invalid nric checksum";
     }
   }
@@ -28,7 +25,7 @@ function nric_checker(nric) {
 
 function nrics_checker(nric_array) {
   nric_array.forEach((element) => {
-    console.log(`Nric: ${element} Status: ${nric_checker(element)}`);
+    console.log(`Nric: ${element} Status: ${nric_checker(element.toUpperCase())}`);
   });
 }
 
